842a510a52068a1f92114f3595514a437dfea1fb
Carlo Centofanti <carlocentos@gmail.com>
2019-06-30 17:49:18 +0200

    Fix collisione Close #51 La collisione è terminata Closes #46

--- src/main/java/it/univaq/dr2/tank19/model/comandi/ComandoFuoco.java
+++ src/main/java/it/univaq/dr2/tank19/model/comandi/ComandoFuoco.java
@@ -23,14 +23,39 @@ public class ComandoFuoco implements Comando {
             Direzione direzione = oggettoDiGioco.getDirezione();
 
             assert proiettile != null;
-            proiettile.setPosY(oggettoDiGioco.getPosY() + 15);
-            proiettile.setPosX(oggettoDiGioco.getPosX() + 15);
+            proiettile.setPosY(getSpawnY(oggettoDiGioco));
+            proiettile.setPosX(getSpawnX(oggettoDiGioco));
 
             proiettile.setDirezione(direzione);
             proiettile.setTank((Tank) oggettoDiGioco);
             oggettoDiGioco.setProiettile(proiettile);
         } else { // TODO: al momento un tank non può sparare più di un proiettile
         }
+    }
+
+    private Integer getSpawnX(OggettoDiGioco oggettoDiGioco) {
+        Integer retVal = oggettoDiGioco.getPosX();
+        if (oggettoDiGioco.getDirezione() == Direzione.EST) retVal = retVal + oggettoDiGioco.getDimensioneHitbox();
+        if (oggettoDiGioco.getDirezione() == Direzione.NORD)
+            retVal = retVal + (oggettoDiGioco.getDimensioneHitbox() / 2);
+        if (oggettoDiGioco.getDirezione() == Direzione.SUD)
+            retVal = retVal + (oggettoDiGioco.getDimensioneHitbox() / 2);
+        if (oggettoDiGioco.getDirezione() == Direzione.OVEST) retVal = retVal - 5;
+
+
+        return retVal;
+    }
+
+    private Integer getSpawnY(OggettoDiGioco oggettoDiGioco) {
+        Integer retVal = oggettoDiGioco.getPosY();
+        if (oggettoDiGioco.getDirezione() == Direzione.SUD) retVal = retVal + oggettoDiGioco.getDimensioneHitbox();
+        if (oggettoDiGioco.getDirezione() == Direzione.EST)
+            retVal = retVal + (oggettoDiGioco.getDimensioneHitbox() / 2);
+        if (oggettoDiGioco.getDirezione() == Direzione.OVEST)
+            retVal = retVal + (oggettoDiGioco.getDimensioneHitbox() / 2);
+        if (oggettoDiGioco.getDirezione() == Direzione.NORD) retVal = retVal - 5;
+
+        return retVal;
     }
 
 
